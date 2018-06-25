55cc7554ae7d320bd77e77495f8277d4febb5468
lauri280 <laurileas@yahoo.com>
2018-06-25 20:20:34 +0300

    debateroom files

--- private/server.js
+++ private/server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-    res.sendFile('index');
+    res.sendFile('debateroom');
 });
 
 
---
+++ public/scripts/scripts.js
@@ -0,0 +1,7 @@
+// Kendo UI listboxide jaoks
+
+let buttonLisa = $("#lisa");
+
+buttonLisa.on("click", function() {
+    console.log("vajutus");
+});
