595fe6636062f25b57ad06d8b240299a9aa350c6
lauri280 <laurileas@yahoo.com>
2018-08-12 18:10:43 +0300

    selection stuff

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,3 +5,19 @@ buttonLisa.on("click", function() {
     let name = $("#name").val();
     $(".people-list").append($(`<li>${name}</li>`));
 });
+
+
+let testNimi = $("#testNimi");
+
+testNimi.on("mouseenter", function() {
+    
+});
+
+testNimi.on("click", function() {
+    if ($(this).css("opacity") == "1") {
+        testNimi.css({"background": "#dfdbdb"});
+    } else {
+        testNimi.css({"background": "none"});
+    }
+    
+});
