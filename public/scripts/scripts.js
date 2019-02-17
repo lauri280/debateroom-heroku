ee8822efd1014f9a5036b132ddb247b1ee8d6ce9
lauri280 <laurileas@yahoo.com>
2019-02-17 18:31:55 +0200

    participant assigning logic

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,6 +1,8 @@
 let osalejad = [];
-let tiimid = []
-let kohtunikud = []
+let vaitlejad = [];
+let tiimid = [];
+let kohtunikud = [];
+
 
 // osaleja nime lisamine "osalejad" array'sse nupu või enter klahviga
 // ja kuvamine osalejate nimekirjas
@@ -16,7 +18,8 @@ $("#lisa").on("click", function() {
     // kuvamine
     $(".people-list").empty();
     osalejad.forEach(element => {
-        $(".people-list").append($(`<li>${element["nimi"]}</li>`));
+        $(".people-list").append($(`<li>${element["nimi"]}</li>`)
+        .addClass("person"));
     });
 
     $("#name").val("");
@@ -31,8 +34,54 @@ $("#name").keypress(function(e) {
 
 // ajutine "määra positioonid" nupu fn
 $("#positsioonid").on("click", function() {
-    console.log(osalejad);
+    console.log(looRuum());
 });
 
 
 // contextMenu, et osalejate nimekirjas olevate nimedega tegeleda
+
+
+// --- Ruumi loomise funktsioonid ---
+
+// tagastab tiimid ja kohtunikud
+function annaRollid (osalejateList) {
+    let newOsalejad = osalejateList.slice();
+    shuffleArray(newOsalejad);
+
+    let tulem = [];
+    let tiimid = [];
+    let kohtunikud = [];
+
+    for (i = 0; i < 4; i++) {
+        let tiim = [];
+
+        tiim.push(newOsalejad.pop());
+        tiim.push(newOsalejad.pop());
+        tiimid.push(tiim);
+    }
+
+    kohtunikud = newOsalejad.slice();
+
+    tulem.push(tiimid);
+    tulem.push(kohtunikud);
+    return tulem;
+}
+
+
+function looRuum () {
+    if (osalejad.length > 9) {
+        let rollid = annaRollid(osalejad)
+        console.log(rollid);
+    }
+}
+
+// stackoverflow'st array shuffle (Durstenfeld shuffle)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// random elemendi leidmine:
+// var rand = myArray[Math.floor(Math.random() * myArray.length)];
